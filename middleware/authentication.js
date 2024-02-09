343e091ff241ecc88d18f205e2a48cc4f4846658
AdekunleM <adekunlemichael1319@gmail.com>
2024-02-09 15:06:59 +0100

    Patient's onboarding

---
+++ middleware/authentication.js
@@ -0,0 +1,60 @@
+const jwt = require('jsonwebtoken');
+const userModel = require('../models/userModel')
+require('dotenv').config();
+
+
+const authenticate =async (req, res, next)=>{
+    try{
+        const hasAuthorization = req.headers.authorization;
+
+
+        // check if user has a token
+        if (!hasAuthorization){
+            return res.status(401).json ({
+                message: "You are not allowed to perform this operation"
+            })
+        }
+        //seperate the token from the bearer
+        const token = hasAuthorization.split(' ')[1];
+        if(!token){
+            return res.status(400).json({
+                message: "Invalid token"
+            })
+        }
+        
+        //decode the token
+        const decodeToken = jwt.verify(token, process.env.jwtSecret); 
+
+        const user = await userModel.findById(decodeToken.userId);
+
+        if (!user) {
+            return res.status(404).json({
+                message: "Account doesn't exist",
+            })
+        }
+        if(user.blacklist.includes(token)){
+            return res.status(400).json({
+                message : "Authorization failed: Please login again",
+            })
+        
+
+        }
+        //pass the payload into the request user
+        req.user = decodeToken
+        next();
+
+    } catch(err){
+        if(err instanceof jwt.JsonWebTokenError){
+            res.status(500).json({
+                message: 'Session timedout, please log in again'
+            })
+            return res.status(500).json({
+                message: "Error authenticating" + err.message
+            })
+            
+        }
+        
+    }
+
+}
+module.exports = authenticate
--- server.js
+++ server.js
@@ -1,11 +1,44 @@
-const express = require("express")
+// const express = require("express")
+// require("./config/config")
+
+// const app = express()
+// app.use(express.json())
+
+// const port = process.env.port
+
+// app.listen(port, ()=> {
+//     console.log(`server is listening on port ${port}`)
+// })
+
+const express = require('express');
+require('dotenv').config();
+
+const cors = require ("cors")
+
+const docRouter = require('./router.js/docMateRoute');
+const fileUpload = require ("express-fileupload")
+const app = express();
+
+app.use(fileUpload({
+    useTempFiles: true,
+    limits: { fileSize: 5 * 1024 * 1024 },
+  }));
+
+
+
+port = process.env.PORT
+
+app.use(express.json());
+
+app.use(cors(``*``))
 require("./config/config")
 
-const app = express()
-app.use(express.json())
-
-const port = process.env.port
-
-app.listen(port, ()=> {
-    console.log(`server is listening on port ${port}`)
+app.get('/', (req, res) => {
+    res.send("Welcome to DocMate");
 })
+
+app.use(docRouter); 
+
+app.listen(port, () => {
+    console.log(`Server running on port: ${port}`);
+});
